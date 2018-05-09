fed08bcf103d2f7fdcb255739936d243c594ce0a
dkgcampbell <dkgcampbell@gmail.com>
2018-05-09 23:19:48 +0800

    refactor: separate component for display of a book shelf

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ListBookShelf from './ListBookShelf';
 
 class ListBooks extends Component {
     render() {
@@ -6,14 +7,17 @@ class ListBooks extends Component {
             {
                 'title': 'To Kill a Mockingbird',
                 'author': 'Harper Lee',
-                'image': 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
+                'image': 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
+                'shelf': 'CurrentlyReading'
             },
             {
                 'title': "Ender's Game",
                 'author': 'Orson Scott Card',
-                'image': 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'
+                'image': 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api',
+                'shelf': 'CurrentlyReading'
             }
         ];
+        const booksCurrentlyReading = books.filter(book => book.shelf === 'CurrentlyReading');
 
         return (
           <div className="list-books">
@@ -22,33 +26,10 @@ class ListBooks extends Component {
           </div>
           <div className="list-books-content">
             <div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {books.map((book) => (
-                        <li>
-                            <div className="book">
-                                <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.image})` }}></div>
-                                <div className="book-shelf-changer">
-                                    <select>
-                                    <option value="none" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                    </select>
-                                </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.author}</div>
-                            </div>
-                        </li>
-                    ))}
-                  </ol>
-                </div>
-              </div>
+              <ListBookShelf
+                books={booksCurrentlyReading}
+                title='Currently Reading'
+              />
               <div className="bookshelf">
                 <h2 className="bookshelf-title">Want to Read</h2>
                 <div className="bookshelf-books">
