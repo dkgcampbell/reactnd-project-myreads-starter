40e123e3591f66567b40c19e1431447c0a161ae8
dkgcampbell <dkgcampbell@gmail.com>
2018-05-20 14:37:43 +0800

    refactor: property types check

--- src/ListBookShelf.js
+++ src/ListBookShelf.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ListBookShelf extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired
+  };
+
   render() {
     return (
       <div className="bookshelf">
